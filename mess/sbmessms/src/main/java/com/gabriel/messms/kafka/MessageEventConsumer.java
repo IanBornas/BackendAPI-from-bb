702a1747aab02ee0a4b90774931c3137373fce0b
Farron1227 <igsbornas@mymail.mapua.edu.ph>
2026-03-10 16:03:41 +0800

    Websocket and HTTP Comms
    yes

--- mess/sbmessms/src/main/java/com/gabriel/messms/kafka/MessageEventConsumer.java
+++ mess/sbmessms/src/main/java/com/gabriel/messms/kafka/MessageEventConsumer.java
@@ -3,16 +3,23 @@ package com.gabriel.messms.kafka;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MessageEventConsumer {
 
-    Logger logger = LoggerFactory.getLogger(MessageEventConsumer.class);
+    private static final Logger logger = LoggerFactory.getLogger(MessageEventConsumer.class);
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public MessageEventConsumer(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
 
     @KafkaListener(topics = KafkaTopicConfig.MESSAGE_CREATED_TOPIC, groupId = "chat-message-group")
     public void consume(MessageCreatedEvent event) {
         logger.info("Received message event: {}", event.toString());
-        // Extend here: push notification, broadcast via WebSocket, update delivery status, etc.
+        String destination = "/topic/conversation/" + event.getConversationId();
+        messagingTemplate.convertAndSend(destination, event);
     }
 }
